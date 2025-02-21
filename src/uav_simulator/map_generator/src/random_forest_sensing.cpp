c93033e85b5a836123c3c87c9439c7ed34053fae
WangJunhao <13188818+wangjunhao0913@user.noreply.gitee.com>
2025-02-21 14:55:57 +0800

    生成随机的固定障碍物

--- src/uav_simulator/map_generator/src/random_forest_sensing.cpp
+++ src/uav_simulator/map_generator/src/random_forest_sensing.cpp
@@ -325,6 +325,150 @@ void RandomMapGenerateCylinder() {
   _map_ok = true;
 }
 
+
+void TestRandomMapGenerate() {
+  // 从 ROS 参数中读取障碍物生成的种子（如果没有则默认使用1）
+  unsigned int seed = 1;
+  ros::NodeHandle nh("~");
+  // nh.param("ObstacleShape/seed", seed, 1u);
+
+  // 使用固定种子初始化随机数引擎
+  std::default_random_engine eng(seed);
+
+  pcl::PointXYZ pt_random;
+  vector<Eigen::Vector2d> obs_position;
+
+  // 使用固定的随机引擎生成均匀分布的随机数
+  rand_x = std::uniform_real_distribution<double>(_x_l, _x_h);
+  rand_y = std::uniform_real_distribution<double>(_y_l, _y_h);
+  rand_w = std::uniform_real_distribution<double>(_w_l, _w_h);
+  rand_h = std::uniform_real_distribution<double>(_h_l, _h_h);
+  rand_inf = std::uniform_real_distribution<double>(0.5, 1.5);
+
+  rand_radius_ = std::uniform_real_distribution<double>(radius_l_, radius_h_);
+  rand_radius2_ = std::uniform_real_distribution<double>(radius_l_, 1.2);
+  rand_theta_ = std::uniform_real_distribution<double>(-theta_, theta_);
+  rand_z_ = std::uniform_real_distribution<double>(z_l_, z_h_);
+
+  // 生成柱状障碍物
+  for (int i = 0; i < _obs_num && ros::ok(); i++) {
+    double x, y, w, h, inf;
+    x = rand_x(eng);
+    y = rand_y(eng);
+    w = rand_w(eng);
+    inf = rand_inf(eng);
+
+    if (sqrt(pow(x - _init_x, 2) + pow(y - _init_y, 2)) < 2.0) {
+      i--;
+      continue;
+    }
+
+    if (sqrt(pow(x - 19.0, 2) + pow(y - 0.0, 2)) < 2.0) {
+      i--;
+      continue;
+    }
+    
+    bool flag_continue = false;
+    for ( auto p : obs_position )
+      if ( (Eigen::Vector2d(x,y) - p).norm() < _min_dist /*metres*/ )
+      {
+        i--;
+        flag_continue = true;
+        break;
+      }
+    if ( flag_continue ) continue;
+
+    obs_position.push_back( Eigen::Vector2d(x,y) );
+    
+    x = floor(x / _resolution) * _resolution + _resolution / 2.0;
+    y = floor(y / _resolution) * _resolution + _resolution / 2.0;
+
+    int widNum = ceil((w*inf) / _resolution);
+    double radius = (w*inf) / 2;
+
+    for (int r = -widNum / 2.0; r < widNum / 2.0; r++)
+      for (int s = -widNum / 2.0; s < widNum / 2.0; s++) {
+        h = rand_h(eng);
+        int heiNum = ceil(h / _resolution);
+        for (int t = -30; t < heiNum; t++) {
+          double temp_x = x + (r + 0.5) * _resolution + 1e-2;
+          double temp_y = y + (s + 0.5) * _resolution + 1e-2;
+          double temp_z = (t + 0.5) * _resolution + 1e-2;
+          if ( (Eigen::Vector2d(temp_x,temp_y) - Eigen::Vector2d(x,y)).norm() <= radius )
+          {
+            pt_random.x = temp_x;
+            pt_random.y = temp_y;
+            pt_random.z = temp_z;
+            cloudMap.points.push_back(pt_random);
+          }
+        }
+      }
+  }
+
+  // 生成圆柱障碍物
+  for (int i = 0; i < circle_num_; ++i) {
+    double x, y, z;
+    x = rand_x(eng);
+    y = rand_y(eng);
+    z = rand_z_(eng);
+
+    if (sqrt(pow(x - _init_x, 2) + pow(y - _init_y, 2)) < 2.0) {
+      i--;
+      continue;
+    }
+
+    if (sqrt(pow(x - 19.0, 2) + pow(y - 0.0, 2)) < 2.0) {
+      i--;
+      continue;
+    }
+
+    x = floor(x / _resolution) * _resolution + _resolution / 2.0;
+    y = floor(y / _resolution) * _resolution + _resolution / 2.0;
+    z = floor(z / _resolution) * _resolution + _resolution / 2.0;
+
+    Eigen::Vector3d translate(x, y, z);
+    double theta = rand_theta_(eng);
+    Eigen::Matrix3d rotate;
+    rotate << cos(theta), -sin(theta), 0.0,
+              sin(theta),  cos(theta), 0.0,
+              0,           0,          1;
+
+    double radius1 = rand_radius_(eng);
+    double radius2 = rand_radius2_(eng);
+
+    // 绘制以 (x,y,z) 为中心的圆形障碍物
+    Eigen::Vector3d cpt;
+    for (double angle = 0.0; angle < 6.282; angle += _resolution / 2) {
+      cpt(0) = 0.0;
+      cpt(1) = radius1 * cos(angle);
+      cpt(2) = radius2 * sin(angle);
+
+      // 膨胀处理
+      Eigen::Vector3d cpt_if;
+      for (int ifx = 0; ifx <= 0; ++ifx)
+        for (int ify = 0; ify <= 0; ++ify)
+          for (int ifz = 0; ifz <= 0; ++ifz) {
+            cpt_if = cpt + Eigen::Vector3d(ifx * _resolution, ify * _resolution, ifz * _resolution);
+            cpt_if = rotate * cpt_if + Eigen::Vector3d(x, y, z);
+            pt_random.x = cpt_if(0);
+            pt_random.y = cpt_if(1);
+            pt_random.z = cpt_if(2);
+            cloudMap.points.push_back(pt_random);
+          }
+    }
+  }
+
+  cloudMap.width = cloudMap.points.size();
+  cloudMap.height = 1;
+  cloudMap.is_dense = true;
+
+  ROS_WARN("Finished generate random map ");
+  kdtreeLocalMap.setInputCloud(cloudMap.makeShared());
+  _map_ok = true;
+}
+
+
+
 //获取无人机当前位置
 void rcvOdometryCallbck(const nav_msgs::Odometry odom) {
   if (odom.child_frame_id == "X" || odom.child_frame_id == "O") return;
@@ -474,7 +618,8 @@ int main(int argc, char** argv) {
   ros::Duration(0.5).sleep();
 
   // RandomMapGenerate();
-  RandomMapGenerateCylinder();
+  // RandomMapGenerateCylinder();
+  TestRandomMapGenerate();
 
   ros::Rate loop_rate(_sense_rate);
 
